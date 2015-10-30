a6dbff626727603911f1a2de1155d9eb1cedf658
fava2026 <fav.narvaez@gmail.com>
2015-10-29 21:02:04 -0300

    promise

--- app/scripts/services/dataResponsive.js
+++ app/scripts/services/dataResponsive.js
@@ -8,53 +8,31 @@
  * Factory in the testProjectApp.
  */
 angular.module('testProjectApp')
-  .factory('dataResponsive', function ($q) {
-  var deferred = $q.defer();
+  .factory('dataResponsive', function ($http, $log) {
 
-  var dataResponsive = [{
-                    "id": 0,
-                    "name": "Favi",
-                    "surname": "Narvaez",
-                    "age": 32,
-                    "address": "Samuel Ramos 67",
-                    "phone": "1234567890"
-                }, {
-                    "id": 1,
-                    "name": "Pablo",
-                    "surname": "Dominguez",
-                    "age": 32,
-                    "address": "Prado Churubusco 30",
-                    "phone": "1234567890"
-                }, {
-                    "id": 2,
-                    "name": "Manuel",
-                    "surname": "Lopez",
-                    "age": 32,
-                    "address": "Sur 67",
-                    "phone": "1234567890"
-                }, {
-                    "id": 3,
-                    "name": "Kena",
-                    "surname": "Caballero",
-                    "age": 32,
-                    "address": "Sonora 67",
-                    "phone": "1234567890"
-                }, {
-                    "id": 4,
-                    "name": "Fabio",
-                    "surname": "Guerra",
-                    "age": 32,
-                    "address": "Heriberto Frias 48",
-                    "phone": "1234567890"
-                }]
+       var elements = {};
 
+       var myService = {
+         getData: function() {
+           var promise = $http.get('../data/dataResponsive.json').then(function (response) {
+             return response.data;
+           });
+           return promise;
+         },
+         getDatas: function(page, limit){
+          var n = (page-1)*limit,
+              rs = [];
 
-    var getData = function() {
-      deferred.resolve(dataResponsive);
-      return deferred.promise;
-    };
+          var promise = $http.get('../data/dataResponsive.json').then(function (response) {
+            angular.forEach(response.data, function (element, index) {
+                if( index >= n)
+                  rs.push(element);
+            });
+            return rs;
+          });
+          return promise;
+         }
+       };
 
-    return {
-      getData: getData
-    };
+       return myService;
   });
